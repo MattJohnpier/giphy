cccec1346bf328ea7fb5e9c81038365cbd2e3a7e
matthew johnpier <mattjohnpier@gmail.com>
2019-03-20 22:20:06 +0000

    doggo

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+  $.ajax({
+      url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+      method: "GET" ,
+      success: function(response) {
+          $(".gallery").append("<img src=" +response.data[7].images.fixed_width.url+ ">")
+      }
+  })
   
   
 });
 
+//"https://giphy.com/gifs/puppy-bed-1TmrEqADCCCS4"
